1ac50bd34c8587835ff472b391ac1079f342e25d
Vaan17 <vaan17440@outlook.fr>
2025-06-03 23:52:26 +0200

    create useUser to use user info everywhere from redux

---
+++ app/javascript/hook/useUser.ts
@@ -0,0 +1,37 @@
+import { useSelector } from "react-redux";
+import createDeepEqualSelector from "../utils/createDeepEqualSelector";
+
+export interface IUser {
+	id: number;
+	nom: string;
+	prenom: string;
+	email: string;
+	genre: string;
+	date_naissance: string;
+	adresse: string;
+	code_postal: string;
+	ville: string;
+	pays: string;
+	telephone: string;
+	categorie_permis: string;
+	lien_image_utilisateur: string;
+	entreprise_id: number;
+	site_id: number;
+	admin_entreprise: boolean;
+	admin_rentecaisse: boolean;
+	derniere_connexion: string;
+	desactive: boolean;
+	date_demande_suppression: string;
+	updated_at: string;
+}
+
+const selectUser = createDeepEqualSelector(
+	(state) => state.user,
+	(user) => user,
+);
+
+const useUser: () => IUser = () => {
+	return useSelector(selectUser) ?? {};
+};
+
+export default useUser;
