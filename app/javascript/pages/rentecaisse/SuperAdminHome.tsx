a41267ae28d7fd2a79cbec3388016c80cd178515
MelvinNumerisk <melvin.aime@numerisk.fr>
2025-09-10 23:00:31 +0200

    j'arrete pas mes commit enfait

--- app/javascript/pages/rentecaisse/SuperAdminHome.tsx
+++ app/javascript/pages/rentecaisse/SuperAdminHome.tsx
@@ -1,24 +1,40 @@
-import { Button, Card, CardContent, Divider, Typography } from '@mui/material'
-import React from 'react'
+import { Button, Card, CardContent, Divider, Grid, Typography } from '@mui/material'
+import React, { useState } from 'react'
 import { Flex } from '../../components/style/flex'
 import { useNavigate } from 'react-router-dom'
+import { isMobile } from 'react-device-detect'
 
 const SuperAdminHome = () => {
     const navigate = useNavigate()
+    const [errorHandlerState, setErrorHandlerState] = useState<{ name: string } | undefined>({ name: "Provoquer une erreur" })
 
     return (
-        <Flex fullWidth alignItemsCenter gap="1em">
-            <Card>
-                <CardContent>
-                    <Flex fullWidth directionColumn gap="1em">
-                        <Typography variant="h5">Utilisateurs</Typography>
-                        <Typography variant="body1">Gérez tous les comptes utilisateurs existant de Rentecaisse</Typography>
-                        <Divider />
-                        <Button variant="contained" color="primary" onClick={() => navigate('/rentecaisse/utilisateurs')}>Gérer les utilisateurs</Button>
-                    </Flex>
-                </CardContent>
-            </Card>
-        </Flex>
+        <Grid container spacing={2}>
+            <Grid item xs={isMobile ? 12 : 4}>
+                <Card>
+                    <CardContent>
+                        <Flex fullWidth directionColumn gap="1em">
+                            <Typography variant="h5">Utilisateurs</Typography>
+                            <Typography variant="body1">Gérez tous les comptes utilisateurs existant de Rentecaisse</Typography>
+                            <Divider />
+                            <Button variant="contained" color="primary" onClick={() => navigate('/rentecaisse/utilisateurs')}>Gérer les utilisateurs</Button>
+                        </Flex>
+                    </CardContent>
+                </Card>
+            </Grid>
+            <Grid item xs={isMobile ? 12 : 4}>
+                <Card>
+                    <CardContent>
+                        <Flex fullWidth directionColumn gap="1em">
+                            <Typography variant="h5">Test ErrorBoundary</Typography>
+                            <Typography variant="body1">Provoquer volontairement une erreur pour tester le composant ErrorBoundary</Typography>
+                            <Divider />
+                            <Button variant="contained" color="error" onClick={() => setErrorHandlerState(undefined)}>{errorHandlerState.name}</Button>
+                        </Flex>
+                    </CardContent>
+                </Card>
+            </Grid>
+        </Grid>
     )
 }
 
